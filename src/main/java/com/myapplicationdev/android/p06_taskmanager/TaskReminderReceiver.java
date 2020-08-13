26cc8d3634fc6e17cb3218ca560bf852cce41af0
18016339 <18016339@myrp.edu.sg>
2020-08-13 21:16:43 +0800

    Initial commit

---
+++ src/main/java/com/myapplicationdev/android/p06_taskmanager/TaskReminderReceiver.java
@@ -0,0 +1,53 @@
+package com.myapplicationdev.android.p06_taskmanager;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+
+public class TaskReminderReceiver extends BroadcastReceiver {
+
+    int notifReqCode = 123;
+
+	@Override
+	public void onReceive(Context context, Intent i) {
+
+		int id = i.getIntExtra("id", -1);
+		String name = i.getStringExtra("name");
+		String desc = i.getStringExtra("desc");
+
+		NotificationManager notificationManager = (NotificationManager)
+				context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			NotificationChannel channel = new
+					NotificationChannel("default", "Default Channel",
+					NotificationManager.IMPORTANCE_DEFAULT);
+
+			channel.setDescription("This is for default notification");
+			notificationManager.createNotificationChannel(channel);
+		}
+
+		Intent intent = new Intent(context, MainActivity.class);
+		PendingIntent pIntent = PendingIntent.getActivity(context, notifReqCode,
+				intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+		// build notification
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+		builder.setContentTitle("Task Manager Reminder");
+		builder.setContentText(name);
+		builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+		builder.setContentIntent(pIntent);
+		builder.setAutoCancel(true);
+
+		Notification n = builder.build();
+
+		notificationManager.notify(notifReqCode, n);
+	}
+
+}
